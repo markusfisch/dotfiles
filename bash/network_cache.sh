ffd3eb2c79b92bc1bf034de889e3c68ebaced802
Markus Fisch <mf@markusfisch.de>
2013-10-17 21:46:01 +0200

    Shortcuts to move files quickly between systems

---
+++ bash/network_cache.sh
@@ -0,0 +1,38 @@
+readonly NETWORK_CACHE='hhsw.de@ssh.strato.de:cache'
+readonly NETWORK_CACHE_OPTIONS='
+	--exclude="*.swp"
+	--recursive
+	--update
+	--delete-after
+	--times
+	--compress'
+
+# Push a file/directory to network cache
+#
+# @param ... - files/directories to push
+push()
+{
+	local FILE
+	for FILE in "$@"
+	do
+		rsync \
+			$NETWORK_CACHE_OPTIONS \
+			"${FILE%/}" \
+			"${NETWORK_CACHE%/}/"
+	done
+}
+
+# Pull a file/directory from network cache
+#
+# @param ... - files/directories to pull
+pull()
+{
+	local FILE
+	for FILE in "$@"
+	do
+		rsync \
+			$NETWORK_CACHE_OPTIONS \
+			"${NETWORK_CACHE%/}/${FILE%/}" \
+			.
+	done
+}
