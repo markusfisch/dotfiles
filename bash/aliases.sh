3314f89dca4331003e266a41245d6bf732387976
Markus Fisch <mf@markusfisch.de>
2015-01-12 21:10:00 +0100

    Clean up

--- bash/aliases.sh
+++ bash/aliases.sh
@@ -12,35 +12,48 @@ alias j='jobs'
 alias e='edit'
 
 # Media playback/viewing
-alias feh='feh --magick-timeout -1'
-alias fs='mplayer -fs -af volume=-30'
+type feh &>/dev/null &&
+	alias feh='feh --magick-timeout -1'
+type mplayer &>/dev/null &&
+	alias fs='mplayer -fs -af volume=-30'
 
 # Recording
-alias rec='ffmpeg -f x11grab -r 20 -s wxga -i :0.0 -sameq -f mp4 -y'
-alias burn='cdrecord dev=/dev/cdrom'
+type ffmpeg &>/dev/null &&
+	alias rec='ffmpeg -f x11grab -r 20 -s wxga -i :0.0 -sameq -f mp4 -y'
+type cdrecord &>/dev/null &&
+	alias burn='cdrecord dev=/dev/cdrom'
 
 # Net
-alias ftp='ftp -pi'
-alias ssh='TERM=xterm ssh'
-alias whois='whois -h whois-servers.net'
-alias htd='python -m SimpleHTTPServer'
+type ftp &>/dev/null &&
+	alias ftp='ftp -pi'
+type ssh &>/dev/null &&
+	alias ssh='TERM=xterm ssh'
+type whois &>/dev/null &&
+	alias whois='whois -h whois-servers.net'
+type python &>/dev/null &&
+	alias htd='python -m SimpleHTTPServer'
 
 # Map BSD/OSX to GNU names
 alias md5sum='md5'
 alias sha1sum='shasum'
-alias ldd='otool -L'
+type otool &>/dev/null &&
+	alias ldd='otool -L'
 
 # Text conversion
-alias utf2iso='iconv -f utf8 -t iso_8859-15'
-alias iso2utf='iconv -f iso_8859-15 -t utf8'
+type iconv &>/dev/null && {
+	alias utf2iso='iconv -f utf8 -t iso_8859-15'
+	alias iso2utf='iconv -f iso_8859-15 -t utf8'
+}
 alias rot13='tr a-zA-Z n-za-mN-ZA-M'
 alias sag='LNG=de say'
 
 # Android
-alias ad='adb devices'
-alias a='and'
-alias ab='a build'
-alias ac='a clean'
+type adb &>/dev/null && {
+	alias ad='adb devices'
+	alias a='and'
+	alias ab='a build'
+	alias ac='a clean'
+}
 
 # Memo
 alias m='memo'
