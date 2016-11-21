127b077105f3082e5ace4ab40adcbf9ae43de731
Markus Fisch <mf@markusfisch.de>
2016-11-21 10:33:13 +0100

    Added mem and dify shortcuts
    And added braces for all type checks.

--- bash/aliases.sh
+++ bash/aliases.sh
@@ -13,42 +13,64 @@ alias h='history'
 alias hc='history -c'
 alias j='jobs'
 
-# Grep
-alias grl='grep -rl'
-alias fgrl='fgrep -rl'
-
 # Editing
 alias e='edit'
 
+# grep
+type grep &>/dev/null && {
+	alias grl='grep -rl'
+	alias fgrl='fgrep -rl'
+}
+
+# diff
+type diff &>/dev/null && {
+	alias dify='diff --suppress-common-lines -wry'
+}
+
+# Show memory for each process
+type ps &>/dev/null && {
+	alias mem='ps ax -o pid,rss,cmd --sort -rss | less -S'
+}
+
 # Media playback/viewing
-type feh &>/dev/null &&
+type feh &>/dev/null && {
 	alias feh='feh --magick-timeout -1'
-type mplayer &>/dev/null &&
+}
+type mplayer &>/dev/null && {
 	alias fs='mplayer -fs -af volume=-30'
+}
 
 # Recording
-type ffmpeg &>/dev/null &&
+type ffmpeg &>/dev/null && {
 	alias rec='ffmpeg -f x11grab -r 20 -s wxga -i :0.0 -sameq -f mp4 -y'
-type cdrecord &>/dev/null &&
+}
+type cdrecord &>/dev/null && {
 	alias burn='cdrecord dev=/dev/cdrom'
+}
 
 # Net
-type ftp &>/dev/null &&
+type ftp &>/dev/null && {
 	alias ftp='ftp -pi'
-type ssh &>/dev/null &&
+}
+type ssh &>/dev/null && {
 	alias ssh='TERM=xterm ssh'
-type whois &>/dev/null &&
+}
+type whois &>/dev/null && {
 	alias whois='whois -h whois-servers.net'
+}
 
 # Map BSD/OSX to GNU names
 alias md5sum='md5'
 alias sha1sum='shasum'
-type otool &>/dev/null &&
+type otool &>/dev/null && {
 	alias ldd='otool -L'
+}
 
 # Text conversion
 type iconv &>/dev/null && {
 	alias utf2iso='iconv -f utf8 -t iso_8859-15'
 	alias iso2utf='iconv -f iso_8859-15 -t utf8'
 }
-alias rot13='tr a-zA-Z n-za-mN-ZA-M'
+type tr  &>/dev/null && {
+	alias rot13='tr a-zA-Z n-za-mN-ZA-M'
+}
