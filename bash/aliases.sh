1459516a42a3256c431a45e57d7b1cc4f2ab5543
Markus Fisch <mf@markusfisch.de>
2017-03-23 11:08:20 +0100

    Added an up command to update && upgrade

--- bash/aliases.sh
+++ bash/aliases.sh
@@ -1,4 +1,4 @@
-# Shortcuts for built-in's
+# shortcuts for built-in's
 alias ..='cd ..'
 alias ...='cd ../..'
 alias ....='cd ../../..'
@@ -13,8 +13,11 @@ alias h='history'
 alias hc='history -c'
 alias j='jobs'
 
-# Editing
+# editing
 alias e='edit'
+
+# updating
+alias up='update && upgrade'
 
 # grep
 type grep &>/dev/null && {
@@ -27,7 +30,7 @@ type diff &>/dev/null && {
 	alias dify='diff --suppress-common-lines -wry'
 }
 
-# Media playback/viewing
+# media playback/viewing
 type feh &>/dev/null && {
 	alias feh='feh --magick-timeout -1'
 }
@@ -35,7 +38,7 @@ type mplayer &>/dev/null && {
 	alias fs='mplayer -fs -af volume=-30'
 }
 
-# Recording
+# recording
 type ffmpeg &>/dev/null && {
 	alias rec='ffmpeg -f x11grab -r 20 -s wxga -i :0.0 -sameq -f mp4 -y'
 }
@@ -43,7 +46,7 @@ type cdrecord &>/dev/null && {
 	alias burn='cdrecord dev=/dev/cdrom'
 }
 
-# Net
+# net
 type ftp &>/dev/null && {
 	alias ftp='ftp -pi'
 }
@@ -54,14 +57,14 @@ type whois &>/dev/null && {
 	alias whois='whois -h whois-servers.net'
 }
 
-# Map BSD/OSX to GNU names
+# map BSD/OSX to GNU names
 alias md5sum='md5'
 alias sha1sum='shasum'
 type otool &>/dev/null && {
 	alias ldd='otool -L'
 }
 
-# Text conversion
+# text conversion
 type iconv &>/dev/null && {
 	alias utf2iso='iconv -f utf8 -t iso_8859-15'
 	alias iso2utf='iconv -f iso_8859-15 -t utf8'
